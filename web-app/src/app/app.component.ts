2bba2abb73a83a0687fb67f19ada13e0f7d0eade
HenryCenturion <u20221a339@upc.edu.pe>
2025-05-15 17:24:05 -0500

    feat: implement loading indicator and authentication check in app component

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import {Component, inject} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, effect, inject, signal} from '@angular/core';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './core/components/header/header.component';
 import {AuthService} from './auth/services/auth.service';
+import {LoadingService} from './core/services/loading.service';
+
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,36 @@ export class AppComponent {
   title = 'dtaquito';
 
   private authService = inject(AuthService);
+  private router = inject(Router);
+  private loadingService = inject(LoadingService);
+
+  isAuthenticated = signal(false);
+  private isNavigating = signal(false);
+  isLoading = signal(false);
 
   constructor() {
-    this.authService.checkAuth();
+    this.authService.isAuthenticated().subscribe((auth) => {
+      this.isAuthenticated.set(auth);
+    });
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.isNavigating.set(true);
+      }
+
+      if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.isNavigating.set(false);
+      }
+    });
+
+    effect(() => {
+      this.isLoading.set(
+        this.isNavigating() || this.loadingService.isLoading()
+      );
+    });
   }
 }
--- web-app/src/app/app.config.ts
+++ web-app/src/app/app.config.ts
@@ -1,12 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
 import {credentialsInterceptor} from './core/interceptors/credentials.interceptor';
+import {loadingInterceptor} from './core/interceptors/loading.interceptor';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideToastr} from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+      closeButton: true
+    }),
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([credentialsInterceptor]))]
+    provideHttpClient(withFetch(), withInterceptors([credentialsInterceptor, loadingInterceptor]))]
 };
