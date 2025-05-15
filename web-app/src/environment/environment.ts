229dac56e4cab68d5428fa16ff8c890be39d4b8b
HenryCenturion <129306649+HenryCenturion@users.noreply.github.com>
2025-05-15 14:59:12 -0500

    chore: delete unnecessary folder

--- web-app/src/environment/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment ={
-  production: false,
-  baseUrl: 'http://localhost:8080/api/v1',
-  chatUrl: 'ws://localhost:8080/ws/chat',
-  locationIQKey: 'pk.57aac1a21ba1a192e5d1c1cc75e88d79'
-}
