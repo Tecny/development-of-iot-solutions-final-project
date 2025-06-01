9e6f0ae82db29b7d16615dfd1f569960599c1f98
HenryCenturion <u20221a339@upc.edu.pe>
2025-06-01 15:14:43 -0500

    style(web): enhance modal component with backdrop and improved styling

--- web-app/src/app/shared/components/modal/modal.component.ts
+++ web-app/src/app/shared/components/modal/modal.component.ts
@@ -1,26 +1,37 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, inject, OnInit, Output, ViewChild} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
 import {CdkPortal, PortalModule} from '@angular/cdk/portal';
 import {Overlay, OverlayConfig} from '@angular/cdk/overlay';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-modal',
-  imports: [PortalModule],
+  imports: [PortalModule, NgStyle],
   template: `
     <ng-template cdkPortal>
-      <div class="modal">
-        <div class="modal__header">
-          <ng-content select="[modal-header]"></ng-content>
-          <button (click)="closeModal.emit()">×</button>
-        </div>
-        <div class="modal__body">
-          <ng-content select="[modal-body]"></ng-content>
-        </div>
-        <div class="modal__footer">
-          <ng-content select="[modal-footer]"></ng-content>
+      <div class="modal-backdrop">
+        <div class="modal" [ngStyle]="{ 'max-width': width }">
+          <div class="modal__header">
+            <ng-content select="[modal-header]"></ng-content>
+            <button (click)="closeModal.emit()">×</button>
+          </div>
+          <div class="modal__body">
+            <ng-content select="[modal-body]"></ng-content>
+          </div>
+          <div class="modal__footer">
+            <ng-content select="[modal-footer]"></ng-content>
+          </div>
         </div>
       </div>
     </ng-template>
-
   `,
   styleUrl: './modal.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -28,6 +39,7 @@ import {Overlay, OverlayConfig} from '@angular/cdk/overlay';
 
 export class ModalComponent implements OnInit {
   @ViewChild(CdkPortal) portal: CdkPortal | undefined;
+  @Input() width: string = '600px';
   @Output() closeModal = new EventEmitter<void>();
 
   overlay = inject(Overlay);
